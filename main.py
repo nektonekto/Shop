6d10898454af7b316371ed37ec2402250637bd8c
prometheus <66139315+nektonekto@users.noreply.github.com>
2022-09-04 15:02:10 +0300

    Add debuger

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Hello():
-    return 'kak'
+def hello():
+    return 'Lolita'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
